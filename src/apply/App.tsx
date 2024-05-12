1f1e8869aafa5385e3fb161e8e37f59dcb3fc556
EH800 <80062347+EH800@users.noreply.github.com>
2024-05-12 05:37:36 -0700

    asd

--- src/apply/App.tsx
+++ src/apply/App.tsx
@@ -1,21 +1,7 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 
-const client = generateClient<Schema>();
-
 function App() {
-  const [Places, setPlaces] = useState<Array<Schema["Places"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Places.observeQuery().subscribe({
-      next: (data) => setPlaces([...data.items]),
-    });
-  }, []);
-
-
 
   return (
         
